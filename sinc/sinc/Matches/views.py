87151f5badd5b692b6d2bdb2e4c8e01733a1e7ac
DAMIANPLATO <driosm@usm.cl>
2024-11-24 01:20:05 -0400

    prueba

--- sinc/sinc/Matches/views.py
+++ sinc/sinc/Matches/views.py
@@ -91,20 +91,20 @@ def match(request):
     matchActual=get_object_or_404(Perfil, usuario_id=matchActual)
     print(matchActual)
     return render(request,'matches.html',{"matchActual":matchActual})
-    matchActual=get_object_or_404(Perfil, id=matchActual)
-    matches = []
-    tipo_contacto=[]
-    nose = madeMatches[0].split(';')
-    del nose[0]
-    for a in nose:
-        a=get_object_or_404(Perfil, id=a)
-        contactos = a.Metodo_contacto.split(';')
-        for contacto in range(len(contactos)):
-            tipo_contacto.append(contactos[contacto].split('.')[1])
-        matches.append({'username':a.username,'platform':tipo_contacto,'url':contactos})
-        tipo_contacto=[]
-    print(f'\n\n\n\n\n\n','matches hechos: ', (madeMatches),'\n\n\n\n')
-    return render(request,'matches.html',{"matchActual":matchActual,'matches':matches})
+    #matchActual=get_object_or_404(Perfil, id=matchActual)
+    #matches = []
+    #tipo_contacto=[]
+    #nose = madeMatches[0].split(';')
+    #del nose[0]
+    #for a in nose:
+        #a=get_object_or_404(Perfil, id=a)
+        #contactos = a.Metodo_contacto.split(';')
+        #for contacto in range(len(contactos)):
+            #tipo_contacto.append(contactos[contacto].split('.')[1])
+        #matches.append({'username':a.username,'platform':tipo_contacto,'url':contactos})
+        #tipo_contacto=[]
+    #print(f'\n\n\n\n\n\n','matches hechos: ', (madeMatches),'\n\n\n\n')
+    #return render(request,'matches.html',{"matchActual":matchActual,'matches':matches})
 
 def match_confirmacion(request, match_id):
     perfil_match = get_object_or_404(Perfil, usuario_id=match_id)
